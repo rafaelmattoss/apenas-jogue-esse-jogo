8f19759d60d27645eefe49a30b98a8e6ae635649
Rafael Matos <rafaelmattos001@gmail.com>
2024-04-02 01:12:11 -0300

    updates

--- funcoes.js
+++ funcoes.js
@@ -2,32 +2,37 @@ $(document).ready(function() {
     
     $('.cartaint').hide();
     $('.cartacon').hide();
-
-   
+    $('.desafio').hide(); // Oculta os desafios inicialmente
+    
     $('#dado').click(function() {
         
         $('#dado').addClass('spin-animation');
 
-        
         setTimeout(function(){
             $('#dado').removeClass('spin-animation');
         }, 1000);
 
         $('.cartaint').hide();
         $('.cartacon').hide();
+        $('.desafio').hide(); // Oculta todos os desafios ao clicar no botão
 
-       
-        var randomIntimidade = Math.floor(Math.random() * $('.cartaint').length);
-        var randomConhecer = Math.floor(Math.random() * $('.cartacon').length);
+        // Gera um número aleatório entre 0 e 99
+        var randomPercent = Math.floor(Math.random() * 100);
 
-      
-        var randomType = Math.random();
-
-        
-        if (randomType < 0.5) {
-            $('.cartaint').eq(randomIntimidade).fadeIn(1000); 
+        // Verifica se o número está dentro dos 10% desejados para exibir um desafio
+        if (randomPercent < 4) {
+            var randomDesafio = Math.floor(Math.random() * $('.desafio').length);
+            $('.desafio').eq(randomDesafio).fadeIn(1000); // Exibe um desafio aleatório
         } else {
-            $('.cartacon').eq(randomConhecer).fadeIn(1000); 
+            var randomIntimidade = Math.floor(Math.random() * $('.cartaint').length);
+            var randomConhecer = Math.floor(Math.random() * $('.cartacon').length);
+            var randomType = Math.random();
+            
+            if (randomType < 0.5) {
+                $('.cartaint').eq(randomIntimidade).fadeIn(1000); 
+            } else {
+                $('.cartacon').eq(randomConhecer).fadeIn(1000); 
+            }
         }
     });
 });
