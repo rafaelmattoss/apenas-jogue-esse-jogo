859c4acc528923a896e8ebf1963064fcddf7946c
Rafael Matos <rafaelmattos001@gmail.com>
2024-04-01 22:01:58 -0300

    algumas animações

--- funcoes.js
+++ funcoes.js
@@ -1,26 +1,33 @@
 $(document).ready(function() {
-    // Esconde todas as cartas no início
+    
     $('.cartaint').hide();
     $('.cartacon').hide();
 
-    // Função para mostrar uma carta aleatória quando o botão for clicado
+   
     $('#dado').click(function() {
-        // Esconde todas as cartas antes de mostrar uma aleatória
+        
+        $('#dado').addClass('spin-animation');
+
+        
+        setTimeout(function(){
+            $('#dado').removeClass('spin-animation');
+        }, 1000);
+
         $('.cartaint').hide();
         $('.cartacon').hide();
 
-        // Gera um número aleatório entre 0 e o número total de cartas de cada tipo
+       
         var randomIntimidade = Math.floor(Math.random() * $('.cartaint').length);
         var randomConhecer = Math.floor(Math.random() * $('.cartacon').length);
 
-        // Gera um número aleatório para decidir qual tipo de carta será mostrada
+      
         var randomType = Math.random();
 
-        // Verifica qual tipo de carta será mostrada e exibe apenas ela
+        
         if (randomType < 0.5) {
-            $('.cartaint').eq(randomIntimidade).show();
+            $('.cartaint').eq(randomIntimidade).fadeIn(1000); 
         } else {
-            $('.cartacon').eq(randomConhecer).show();
+            $('.cartacon').eq(randomConhecer).fadeIn(1000); 
         }
     });
 });
