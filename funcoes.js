4cdd34aad565b5ff7453003498bd9180a279a5b2
Rafael Matos <rafaelmattos001@gmail.com>
2024-04-03 20:19:44 -0300

    updates

--- funcoes.js
+++ funcoes.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     $('.cartacon').hide();
     $('.desafio').hide(); // Oculta os desafios inicialmente
     
+    var divsExibidas = []; // Array para armazenar os índices das divs já exibidas
+    
     $('#dado').click(function() {
         
         $('#dado').addClass('spin-animation');
@@ -21,18 +23,32 @@ $(document).ready(function() {
 
         // Verifica se o número está dentro dos 10% desejados para exibir um desafio
         if (randomPercent < 3) {
-            var randomDesafio = Math.floor(Math.random() * $('.desafio').length);
+            var randomDesafio = getRandomIndex('.desafio', divsExibidas);
             $('.desafio').eq(randomDesafio).fadeIn(1000); // Exibe um desafio aleatório
+            divsExibidas.push(randomDesafio);
         } else {
-            var randomIntimidade = Math.floor(Math.random() * $('.cartaint').length);
-            var randomConhecer = Math.floor(Math.random() * $('.cartacon').length);
+            var randomIntimidade = getRandomIndex('.cartaint', divsExibidas);
+            var randomConhecer = getRandomIndex('.cartacon', divsExibidas);
             var randomType = Math.random();
             
             if (randomType < 0.5) {
                 $('.cartaint').eq(randomIntimidade).fadeIn(1000); 
+                divsExibidas.push(randomIntimidade);
             } else {
                 $('.cartacon').eq(randomConhecer).fadeIn(1000); 
+                divsExibidas.push(randomConhecer);
             }
         }
     });
+
+    // Função para obter um índice aleatório não exibido
+    function getRandomIndex(selector, displayedIndexes) {
+        var indexes = [];
+        $(selector).each(function(index) {
+            if (!displayedIndexes.includes(index)) {
+                indexes.push(index);
+            }
+        });
+        return indexes[Math.floor(Math.random() * indexes.length)];
+    }
 });
