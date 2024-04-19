022bafd2c203af35d80d439aff14afbc4955236d
Rafael Matos <rafaelmattos001@gmail.com>
2024-04-19 15:16:33 -0300

    usei o firebase com sucesso

--- validacao.js
+++ validacao.js
@@ -1,21 +1,27 @@
+$('#entrar').click(function() {
+    
+    var email = $('#email').val();
+    var senha = $('#senha').val();
+
+
+    if (!email && !senha) {
+        alert("Todos os campos devem ser preenchidos.");
+    } else if (!email) {
+        alert("O campo de E-mail é obrigatório.");
+    } else if (!senha) {
+        alert("O campo de Senha é obrigatório.");
+    } else {
+        window.location.href = 'jogo.html';
+    }
+});
+
+const form ={
+    email: document.querySelector('#email'),
+}
+
 
             
-            let botaoentrar = $("#entrar")
-            botaoentrar.click(validacaodeuser)
-
-
-            function validacaodeuser(){
-                let usuario = $("#usuario").val();
-                let senha = $("#senha").val();
-                let erro =$("#erro");
-                
-                if(!usuario || !senha){
-                    erro.show();
-                }
-
-            }
-  
-            
+               
 
     
     
