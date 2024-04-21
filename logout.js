0e7f630e075f466069109eb81d371e80186bfcae
Rafael Matos <rafaelmattos001@gmail.com>
2024-04-21 02:32:21 -0300

    new

--- logout.js
+++ logout.js
@@ -1,19 +1,12 @@
-
-$("#sair").click(logOut)
+$("#sair").click(logOut);
 
 function logOut() {
-    if (firebase.auth().currentUser) {
-        
-        firebase.auth().signOut()
-            .then(() => {
-                window.location.href = 'index.html';
-            })
-            .catch((error) => {
-                
-                alert('Erro ao fazer logout. Por favor, tente novamente.');
-            });
-    } else {
-       
-        window.location.href = 'index.html';
-    }
+    firebase.auth().signOut()
+        .then(() => {
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
+            alert('Erro ao fazer logout. Por favor, tente novamente.');
+        });
 }
+
