99694fe407e6d93225794e65959fc0e3052ac78b
Rafael Matos <rafaelmattos001@gmail.com>
2024-06-24 00:59:55 -0300

    novas atts

--- date.js
+++ date.js
@@ -1,32 +1,36 @@
-
 $(document).ready(function() {
-
-    // Oculta todas as divs com a classe 'familia'
-    $(".familia").hide();
+    // Oculta todas as divs com a classe 'date'
+    $(".date").hide();
     $("#fechar").hide();
     $("#normal").hide();
     
+    // Array para manter o controle das cartas já exibidas
+    let exibidas = [];
 
     // Manipulação do clique no botão "Rode o Dado"
     $('#dado').click(function() {
-        
-
         $(this).addClass('spin-animation');
 
         setTimeout(() => {
             $(this).removeClass('spin-animation');
         }, 1000);
 
-    
         $(".date").hide();
 
         var $cartas = $('.date:not(:visible)');
         
         // Verifica se há alguma div disponível para mostrar
         if ($cartas.length > 0) {
+            var randomCarta;
             
-            var randomCarta = Math.floor(Math.random() * $cartas.length);
+            do {
+                randomCarta = Math.floor(Math.random() * $cartas.length);
+            } while (exibidas.includes(randomCarta) && exibidas.length < $cartas.length);
             
+            // Adiciona a carta escolhida à lista de exibidas
+            exibidas.push(randomCarta);
+            
+            // Mostra a carta
             $cartas.eq(randomCarta).fadeIn(1000);
         } else {
             console.log("Todas as cartas já foram exibidas!");
@@ -35,18 +39,16 @@ $(document).ready(function() {
 });
 
 $('#config').click(function() {
-    $('#config').hide()
-    $("#fechar").show()
+    $('#config').hide();
+    $("#fechar").show();
     $('#menuconfig').show(); // Alterna a visibilidade do menuconfig
-})
+});
 
 $('#fechar').click(function() {
-    $('#config').show()
-    $("#fechar").hide()
+    $('#config').show();
+    $("#fechar").hide();
     $('#menuconfig').hide(); // Alterna a visibilidade do menuconfig
-})
-
-
+});
 
 $("#suaconta").click(() => {
     window.location.href = 'perfil.html';
---
+++ home.js
@@ -0,0 +1,11 @@
+$("#classico").click(function() {
+    window.location.href = 'jogo.html';
+});
+
+$("#familia").click(function() {
+    window.location.href = 'familia.html';
+});
+
+$("#date").click(function() {
+    window.location.href = 'date.html';
+});
