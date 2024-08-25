f70b64ab1b8e14397dbe8919811b3658b782dfa2
Rafael Matos <rafaelmattos001@gmail.com>
2024-08-25 20:13:19 -0300

    s

--- functions_v2/index.js
+++ functions_v2/index.js
@@ -1,16 +1,4 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-const functions = require('firebase-functions');
+const functions = require('firebase-functions/v2');
 const admin = require('firebase-admin');
 const stripe = require('stripe')(functions.config().stripe.secret);
 
@@ -25,7 +13,7 @@ exports.createCheckoutSession = functions.https.onCall(async (data, context) => 
                 product_data: {
                     name: 'Muito Mais Que Um Jogo',
                 },
-                unit_amount: 2000, // em centavos (20,00 USD)
+                unit_amount: 2000, // em centavos (20,00 BRL)
             },
             quantity: 1,
         }],
